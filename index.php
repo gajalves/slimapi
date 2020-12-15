7c6fcfb5b265ad1774a519d3be738c156a26aa63
joimo <ga.jaimealves@gmail.com>
2020-12-15 20:21:49 -0300

    agrupar rotas

--- index.php
+++ index.php
@@ -22,5 +22,35 @@
         echo $id;
     });
 
+    $app->get('/lista/{itens:.*}', function($request, $response){
+        $itens = $request->getAttribute('itens');
+        echo $itens;
+    });
+
+
+    /* nomear rotas */
+
+    $app->get('/blog/fodase/{id}', function($request, $response){
+        echo 'Lista fodase';
+    })->setName('fodase');
+
+
+    $app->get('/testfodase', function($request, $response){
+        $retorno = $this->get("router")->pathFor("fodase", ['id' => '10']);
+
+        //echo $retorno;
+    });
+
+    /* agrupar rotas */
+
+    $app->group('/v1', function(){
+        $this->get('/usuarios', function($request, $response){
+            echo 'lista usuarios';        
+        });            
+
+        $this->get('/postagens', function($request, $response){
+            echo 'lista postagens';        
+        });
+    });       
 
     $app->run();
