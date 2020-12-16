34aaf2da68ba7a8a6c4566ac8d37733b0c8aef08
joimo <ga.jaimealves@gmail.com>
2020-12-15 21:11:03 -0300

    psr7

--- index.php
+++ index.php
@@ -7,50 +7,71 @@
     require __DIR__ . '/vendor/autoload.php';
 
     $app = new Slim\App;
+    
+    //padrao psr7
 
-    $app->get('/json', function(){
-        $data = ['name' => 'gabriel', 'age' => '22'];
-        echo json_encode($data);
+    $app->get('/postagens', function(Request $request, Response $response){
+        
+        $response->getBody()->write("lista essa porra");        
+        return $response;
     });
-
-    $app->get('/postagens', function(){
-        echo 'Lista de posts';
-    });
-
-    $app->get('/usuarios/{id}', function($request, $response){
-        $id = $request->getAttribute('id');
-        echo $id;
-    });
-
-    $app->get('/lista/{itens:.*}', function($request, $response){
-        $itens = $request->getAttribute('itens');
-        echo $itens;
-    });
-
-
-    /* nomear rotas */
-
-    $app->get('/blog/fodase/{id}', function($request, $response){
-        echo 'Lista fodase';
-    })->setName('fodase');
-
-
-    $app->get('/testfodase', function($request, $response){
-        $retorno = $this->get("router")->pathFor("fodase", ['id' => '10']);
-
-        //echo $retorno;
-    });
-
-    /* agrupar rotas */
-
-    $app->group('/v1', function(){
-        $this->get('/usuarios', function($request, $response){
-            echo 'lista usuarios';        
-        });            
-
-        $this->get('/postagens', function($request, $response){
-            echo 'lista postagens';        
-        });
-    });       
 
     $app->run();
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // // $app->get('/json', function(){
+    // //     $data = ['name' => 'gabriel', 'age' => '22'];
+    // //     echo json_encode($data);
+    // // });
+
+    // // $app->get('/postagens', function(){
+    // //     echo 'Lista de posts';
+    // // });
+
+    // // $app->get('/usuarios/{id}', function($request, $response){
+    // //     $id = $request->getAttribute('id');
+    // //     echo $id;
+    // // });
+
+    // // $app->get('/lista/{itens:.*}', function($request, $response){
+    // //     $itens = $request->getAttribute('itens');
+    // //     echo $itens;
+    // // });
+
+
+    // // /* nomear rotas */
+
+    // // $app->get('/blog/fodase/{id}', function($request, $response){
+    // //     echo 'Lista fodase';
+    // // })->setName('fodase');
+
+
+    // // $app->get('/testfodase', function($request, $response){
+    // //     $retorno = $this->get("router")->pathFor("fodase", ['id' => '10']);
+
+    // //     //echo $retorno;
+    // // });
+
+    // // /* agrupar rotas */
+
+    // // $app->group('/v1', function(){
+    // //     $this->get('/usuarios', function($request, $response){
+    // //         echo 'lista usuarios';        
+    // //     });            
+
+    // //     $this->get('/postagens', function($request, $response){
+    // //         echo 'lista postagens';        
+    // //     });
+    // // });               
+    
