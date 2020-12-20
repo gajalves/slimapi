65fa83ea8044ce980ef979c577dbdc6aad0cd51c
joimo <ga.jaimealves@gmail.com>
2020-12-20 12:42:37 -0300

    middleware

---
+++ index-old.php
@@ -0,0 +1,119 @@
+<?php
+
+    use Psr\Http\Message\ResponseInterface as Response;
+    use Psr\Http\Message\ServerRequestInterface as Request;
+    use Slim\Factory\AppFactory;
+
+    require __DIR__ . '/vendor/autoload.php';
+    require 'class.php';
+
+    $app = new Slim\App;
+
+    //$servico = new Servico;
+
+    //injeção de dependencia
+    //container pimple
+    $container = $app->getContainer();
+    $container['servico'] = function() {
+        return new Servico;
+    };
+
+    $app->get('/servico', function(Request $request, Response $response) {       
+      $servico = $this->get('servico');
+      var_dump($servico);
+        
+    });
+
+
+    /** Controllers como serviço */
+    $app->get('/usuario', '\MyApp\controllers\Home:Index');
+
+
+
+    $app->run();
+
+
+
+
+    /*
+    //padrao psr7
+    $app->get('/postagens', function(Request $request, Response $response){       
+        $response->getBody()->write("lista essa porra");        
+        return $response;
+    });
+    
+    //http methods
+    $app->get('/posts', function(Request $request, Response $response){
+        $response->getBody()->write("lista de posts");        
+        return $response;
+    });
+
+    $app->post('/usuarios/add', function(Request $request, Response $response){       
+
+        //recupera post
+        $post = $request->getParsedBody();
+        $nome = $post['nome'];
+        $email = $post['email'];
+
+        return $response->getBody()->write($nome);
+    });
+    
+    $app->put('/usuarios/edit', function(Request $request, Response $response) {
+        $post = $request->getParsedBody();
+        $id = $post['id'];
+        $nome = $post['nome'];
+
+        return $response->getBody()->write("Sucesso ao atualizar o id: " . $id);
+    });
+
+    $app->delete('/usuarios/remove/{id}', function(Request $request, Response $response) {
+        $id = $request->getAttribute('id');
+        
+        return $response->getBody()->write("Sucesso ao remover o id: " . $id);
+    }); */
+    
+    // // $app->get('/json', function(){
+    // //     $data = ['name' => 'gabriel', 'age' => '22'];
+    // //     echo json_encode($data);
+    // // });
+
+    // // $app->get('/postagens', function(){
+    // //     echo 'Lista de posts';
+    // // });
+
+    // // $app->get('/usuarios/{id}', function($request, $response){
+    // //     $id = $request->getAttribute('id');
+    // //     echo $id;
+    // // });
+
+    // // $app->get('/lista/{itens:.*}', function($request, $response){
+    // //     $itens = $request->getAttribute('itens');
+    // //     echo $itens;
+    // // });
+
+
+    // // /* nomear rotas */
+
+    // // $app->get('/blog/fodase/{id}', function($request, $response){
+    // //     echo 'Lista fodase';
+    // // })->setName('fodase');
+
+
+    // // $app->get('/testfodase', function($request, $response){
+    // //     $retorno = $this->get("router")->pathFor("fodase", ['id' => '10']);
+
+    // //     //echo $retorno;
+    // // });
+
+    // // /* agrupar rotas */
+
+    // // $app->group('/v1', function(){
+    // //     $this->get('/usuarios', function($request, $response){
+    // //         echo 'lista usuarios';        
+    // //     });            
+
+    // //     $this->get('/postagens', function($request, $response){
+    // //         echo 'lista postagens';        
+    // //     });
+    // // });               
+    
--- index.php
+++ index.php
@@ -9,111 +9,44 @@
 
     $app = new Slim\App;
 
-    //$servico = new Servico;
+    
 
-    //injeção de dependencia
-    //container pimple
-    $container = $app->getContainer();
-    $container['servico'] = function() {
-        return new Servico;
-    };
+    $app->get('/json', function(Request $req, Response $res) {       
+        //return $res->write("Header return");
+        // return $res->withJson([
+        //     "nome" => "Gabriel Jaime",
+        //     "bonito" => "muito"
+        // ]);
 
-    $app->get('/servico', function(Request $request, Response $response) {       
-      $servico = $this->get('servico');
-      var_dump($servico);
-        
+
+       
     });
 
 
-    /** Controllers como serviço */
-    $app->get('/usuario', '\MyApp\controllers\Home:Index');
+
+    /** Middleware */
+
+    $app->add(function($req, $res, $next) {
+        $res->write("middleware - camada 1 + "); 
+        //return $next($req, $res);
+        $res = $next($req, $res);
+        $res->write(' + Fim da camada 1');
+        return $res;
+    });
+
+    // $app->add(function($req, $res, $next) {
+    //     $res->write("middleware - camada 2 + "); 
+    //     return $next($req, $res);
+    // });
 
 
+    $app->get('/usuarios', function(Request $req, Response $res) {       
+        return $res->write("Lista usuarios");
+    });
+
+    $app->get('/posts', function(Request $req, Response $res) {       
+        return $res->write("Lista posts");
+    });
 
     $app->run();
 
-
-
-
-    /*
-    //padrao psr7
-    $app->get('/postagens', function(Request $request, Response $response){       
-        $response->getBody()->write("lista essa porra");        
-        return $response;
-    });
-    
-    //http methods
-    $app->get('/posts', function(Request $request, Response $response){
-        $response->getBody()->write("lista de posts");        
-        return $response;
-    });
-
-    $app->post('/usuarios/add', function(Request $request, Response $response){       
-
-        //recupera post
-        $post = $request->getParsedBody();
-        $nome = $post['nome'];
-        $email = $post['email'];
-
-        return $response->getBody()->write($nome);
-    });
-    
-    $app->put('/usuarios/edit', function(Request $request, Response $response) {
-        $post = $request->getParsedBody();
-        $id = $post['id'];
-        $nome = $post['nome'];
-
-        return $response->getBody()->write("Sucesso ao atualizar o id: " . $id);
-    });
-
-    $app->delete('/usuarios/remove/{id}', function(Request $request, Response $response) {
-        $id = $request->getAttribute('id');
-        
-        return $response->getBody()->write("Sucesso ao remover o id: " . $id);
-    }); */
-    
-    // // $app->get('/json', function(){
-    // //     $data = ['name' => 'gabriel', 'age' => '22'];
-    // //     echo json_encode($data);
-    // // });
-
-    // // $app->get('/postagens', function(){
-    // //     echo 'Lista de posts';
-    // // });
-
-    // // $app->get('/usuarios/{id}', function($request, $response){
-    // //     $id = $request->getAttribute('id');
-    // //     echo $id;
-    // // });
-
-    // // $app->get('/lista/{itens:.*}', function($request, $response){
-    // //     $itens = $request->getAttribute('itens');
-    // //     echo $itens;
-    // // });
-
-
-    // // /* nomear rotas */
-
-    // // $app->get('/blog/fodase/{id}', function($request, $response){
-    // //     echo 'Lista fodase';
-    // // })->setName('fodase');
-
-
-    // // $app->get('/testfodase', function($request, $response){
-    // //     $retorno = $this->get("router")->pathFor("fodase", ['id' => '10']);
-
-    // //     //echo $retorno;
-    // // });
-
-    // // /* agrupar rotas */
-
-    // // $app->group('/v1', function(){
-    // //     $this->get('/usuarios', function($request, $response){
-    // //         echo 'lista usuarios';        
-    // //     });            
-
-    // //     $this->get('/postagens', function($request, $response){
-    // //         echo 'lista postagens';        
-    // //     });
-    // // });               
-    
